8e2c3c89f2ddec1a2d8cabd7c355c0c7e641ce59
skejeton <ishidex2@gmail.com>
2023-12-02 09:08:36 -0300

    advent 1 and 2

--- external/jumbo.c
+++ external/jumbo.c
@@ -1,1 +1,69 @@
 // Jumbo of all external C files.
+#if 0
+#define UMKA_STATIC
+#define IS_EMSCRIPTEN
+#include "umka_api.h" 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#if defined(IS_EMSCRIPTEN)
+#include <emscripten.h>
+#endif
+
+// fn localstorage_get(): str
+UMKA_EXPORT void
+localstorage_get(UmkaStackSlot *p, UmkaStackSlot *r)
+{
+#if defined(IS_EMSCRIPTEN)
+  void *dat = calloc(1, 1024);
+
+  EM_ASM({
+    var str = localStorage.getItem('localstorage');
+    if (str === null)
+      str = '';
+    var dat = (new TextEncoder()).encode(str);
+    setValue($0, dat, 'i32');
+  }, dat);
+  p->ptrVal = umkaMakeStr(umka, buf);
+  free(buf);
+#else
+  FILE *f = fopen("localstorage.txt", "rb");
+  void *umka = r->ptrVal;
+
+  if (f == NULL)
+  {
+    r->ptrVal = umkaMakeStr(umka, "");
+    return;
+  }
+ 
+  fseek(f, 0, SEEK_END);
+  int size = ftell(f);
+  fseek(f, 0, SEEK_SET);
+
+  char *buf = calloc(size + 1, 1);
+  fread(buf, 1, size, f);
+
+  fclose(f);
+
+  r->ptrVal = umkaMakeStr(umka, buf);
+  free(buf);
+#endif
+}
+
+// fn localstorage_set(s: str)
+UMKA_EXPORT void
+localstorage_set(UmkaStackSlot *p, UmkaStackSlot *r)
+{
+#if defined(IS_EMSCRIPTEN)
+  EM_ASM({
+    localStorage.setItem('localstorage', $0);
+  }, p->ptrVal);
+#else
+  FILE *f = fopen("localstorage.txt", "wb");
+  char *buf = p->ptrVal;
+
+  fwrite(buf, 1, strlen(buf), f);
+  fclose(f);
+#endif
+}
+#endif
--- external/umka_api.h
+++ external/umka_api.h
@@ -1,112 +1,103 @@
 #ifndef UMKA_API_H_INCLUDED
 #define UMKA_API_H_INCLUDED
 
-
 #ifdef _WIN32
-    #if defined(UMKA_STATIC)
-        #define UMKA_EXPORT
-        #define UMKA_IMPORT
-    #else
-        #define UMKA_EXPORT __declspec(dllexport)
-        #define UMKA_IMPORT __declspec(dllimport)
-    #endif
+#if defined(UMKA_STATIC)
+#define UMKA_EXPORT
+#define UMKA_IMPORT
 #else
-    #define UMKA_EXPORT __attribute__((visibility("default")))
-    #define UMKA_IMPORT __attribute__((visibility("default")))
+#define UMKA_EXPORT __declspec(dllexport)
+#define UMKA_IMPORT __declspec(dllimport)
+#endif
+#else
+#define UMKA_EXPORT __attribute__((visibility("default")))
+#define UMKA_IMPORT __attribute__((visibility("default")))
 #endif
 
 #ifdef UMKA_BUILD
-    #define UMKA_API UMKA_EXPORT
+#define UMKA_API UMKA_EXPORT
 #else
-    #define UMKA_API UMKA_IMPORT
+#define UMKA_API UMKA_IMPORT
 #endif
 
-
-#include <stdint.h>
 #include <stdbool.h>
-
+#include <stdint.h>
 
 #if defined(__cplusplus)
 extern "C" {
 #endif
 
-
-typedef union
-{
-    int64_t intVal;
-    uint64_t uintVal;
-    void *ptrVal;
-    double realVal;
-    float real32Val;
+typedef union {
+  int64_t intVal;
+  uint64_t uintVal;
+  void *ptrVal;
+  double realVal;
+  float real32Val;
 } UmkaStackSlot;
-
 
 typedef void (*UmkaExternFunc)(UmkaStackSlot *params, UmkaStackSlot *result);
 
-
-typedef enum
-{
-    UMKA_HOOK_CALL,
-    UMKA_HOOK_RETURN,
+typedef enum {
+  UMKA_HOOK_CALL,
+  UMKA_HOOK_RETURN,
 } UmkaHookEvent;
 
+typedef void (*UmkaHookFunc)(const char *fileName, const char *funcName,
+                             int line);
 
-typedef void (*UmkaHookFunc)(const char *fileName, const char *funcName, int line);
+#define UmkaDynArray(T)                                                        \
+  struct {                                                                     \
+    void *internal;                                                            \
+    int64_t itemSize;                                                          \
+    T *data;                                                                   \
+  }
 
-
-#define UmkaDynArray(T) struct \
-{ \
-    void *internal; \
-    int64_t len; \
-    int64_t itemSize; \
-    T *data; \
-}
-
-
-typedef struct
-{
-    void *internal1;
-    void *internal2;
+typedef struct {
+  void *internal1;
+  void *internal2;
 } UmkaMap;
 
+enum { UMKA_MSG_LEN = 255 };
 
-enum
-{
-    UMKA_MSG_LEN = 255
-};
-
-
-typedef struct
-{
-    char fileName[UMKA_MSG_LEN + 1];
-    char fnName[UMKA_MSG_LEN + 1];
-    int line, pos;
-    char msg[UMKA_MSG_LEN + 1];
+typedef struct {
+  char fileName[UMKA_MSG_LEN + 1];
+  char fnName[UMKA_MSG_LEN + 1];
+  int line, pos;
+  char msg[UMKA_MSG_LEN + 1];
 } UmkaError;
-
 
 typedef void (*UmkaWarningCallback)(UmkaError *warning);
 
-
-UMKA_API void *umkaAlloc            (void);
-UMKA_API bool umkaInit              (void *umka, const char *fileName, const char *sourceString, int stackSize, const char *locale, int argc, char **argv, bool fileSystemEnabled, bool implLibsEnabled, UmkaWarningCallback warningCallback);
-UMKA_API bool umkaCompile           (void *umka);
-UMKA_API bool umkaRun               (void *umka);
-UMKA_API bool umkaCall              (void *umka, int entryOffset, int numParamSlots, UmkaStackSlot *params, UmkaStackSlot *result);
-UMKA_API void umkaFree              (void *umka);
-UMKA_API void umkaGetError          (void *umka, UmkaError *err);
-UMKA_API void umkaAsm               (void *umka, char *buf, int size);
-UMKA_API bool umkaAddModule         (void *umka, const char *fileName, const char *sourceString);
-UMKA_API bool umkaAddFunc           (void *umka, const char *name, UmkaExternFunc func);
-UMKA_API int  umkaGetFunc           (void *umka, const char *moduleName, const char *funcName);
-UMKA_API bool umkaGetCallStack      (void *umka, int depth, int *offset, char *name, int size);
-UMKA_API void umkaSetHook           (void *umka, UmkaHookEvent event, UmkaHookFunc hook);
-UMKA_API void *umkaAllocData        (void *umka, int size, UmkaExternFunc onFree);
-UMKA_API void umkaIncRef            (void *umka, void *ptr);
-UMKA_API void umkaDecRef            (void *umka, void *ptr);
-UMKA_API void *umkaGetMapItem       (void *umka, UmkaMap *map, UmkaStackSlot key);
-UMKA_API const char *umkaGetVersion (void);
-
+UMKA_API void *umkaAlloc(void);
+UMKA_API bool umkaInit(void *umka, const char *fileName,
+                       const char *sourceString, int stackSize,
+                       const char *locale, int argc, char **argv,
+                       bool fileSystemEnabled, bool implLibsEnabled,
+                       UmkaWarningCallback warningCallback);
+UMKA_API bool umkaCompile(void *umka);
+UMKA_API bool umkaRun(void *umka);
+UMKA_API bool umkaCall(void *umka, int entryOffset, int numParamSlots,
+                       UmkaStackSlot *params, UmkaStackSlot *result);
+UMKA_API void umkaFree(void *umka);
+UMKA_API void umkaGetError(void *umka, UmkaError *err);
+UMKA_API char *umkaAsm(void *umka);
+UMKA_API bool umkaAddModule(void *umka, const char *fileName,
+                            const char *sourceString);
+UMKA_API bool umkaAddFunc(void *umka, const char *name, UmkaExternFunc func);
+UMKA_API int umkaGetFunc(void *umka, const char *moduleName,
+                         const char *funcName);
+UMKA_API bool umkaGetCallStack(void *umka, int depth, int nameSize, int *offset,
+                               char *fileName, char *fnName, int *line);
+UMKA_API void umkaSetHook(void *umka, UmkaHookEvent event, UmkaHookFunc hook);
+UMKA_API void *umkaAllocData(void *umka, int size, UmkaExternFunc onFree);
+UMKA_API void umkaIncRef(void *umka, void *ptr);
+UMKA_API void umkaDecRef(void *umka, void *ptr);
+UMKA_API void *umkaGetMapItem(void *umka, UmkaMap *map, UmkaStackSlot key);
+UMKA_API char *umkaMakeStr(void *umka, const char *str);
+UMKA_API int umkaGetStrLen(const char *str);
+UMKA_API void umkaMakeDynArray(void *umka, void *array, void *type, int len);
+UMKA_API int umkaGetDynArrayLen(const void *array);
+UMKA_API const char *umkaGetVersion(void);
 
 #if defined(__cplusplus)
 }
