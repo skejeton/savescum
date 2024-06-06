46a4a0b003d8a5ec3e7a34911858d34bed91ff39
skejeton <ishidex2@gmail.com>
2024-06-06 10:53:29 -0300

    Optimizations

--- external/localstorage.c
+++ external/localstorage.c
@@ -1,38 +1,37 @@
 #define UMKA_STATIC
-#include "umka_api.h" 
+#include "umka_api.h"
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #if defined(__EMSCRIPTEN__)
 #include <emscripten.h>
 #endif
 
 // fn localstorage_get(): str
-UMKA_EXPORT void
-localstorage_get(UmkaStackSlot *p, UmkaStackSlot *r)
-{
+UMKA_EXPORT void localstorage_get(UmkaStackSlot *p, UmkaStackSlot *r) {
 #if defined(__EMSCRIPTEN__)
   void *dat = calloc(1, 1024);
 
-  EM_ASM({
-    var str = localStorage.getItem('localstorage');
-    if (str === null)
-      str = '';
-    var dat = (new TextEncoder()).encode(str);
-    setValue($0, dat, 'i32');
-  }, dat);
+  EM_ASM(
+      {
+        var str = localStorage.getItem('localstorage');
+        if (str == = null)
+          str = '';
+        var dat = (new TextEncoder()).encode(str);
+        setValue($0, dat, 'i32');
+      },
+      dat);
   p->ptrVal = umkaMakeStr(umka, buf);
   free(buf);
 #else
   FILE *f = fopen("localstorage.txt", "rb");
   void *umka = r->ptrVal;
 
-  if (f == NULL)
-  {
+  if (f == NULL) {
     r->ptrVal = umkaMakeStr(umka, "");
     return;
   }
- 
+
   fseek(f, 0, SEEK_END);
   int size = ftell(f);
   fseek(f, 0, SEEK_SET);
@@ -48,13 +47,9 @@ localstorage_get(UmkaStackSlot *p, UmkaStackSlot *r)
 }
 
 // fn localstorage_set(s: str)
-UMKA_EXPORT void
-localstorage_set(UmkaStackSlot *p, UmkaStackSlot *r)
-{
+UMKA_EXPORT void localstorage_set(UmkaStackSlot *p, UmkaStackSlot *r) {
 #if defined(__EMSCRIPTEN__)
-  EM_ASM({
-    localStorage.setItem('localstorage', $0);
-  }, p->ptrVal);
+  EM_ASM({ localStorage.setItem('localstorage', $0); }, p->ptrVal);
 #else
   FILE *f = fopen("localstorage.txt", "wb");
   char *buf = p->ptrVal;
